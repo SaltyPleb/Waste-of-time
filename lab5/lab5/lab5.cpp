c70e69230cac11d60dbea9618d0252623c2b0981
Yauheni Bulyha <48885368+SaltyPleb@users.noreply.github.com>
2020-09-17 17:25:52 +0300

    Add files via upload

---
+++ lab5/lab5/lab5.cpp
@@ -0,0 +1,228 @@
+﻿#include <iostream>
+
+#include <conio.h>
+#include <string>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int end() { cout << "\nend\n"; _getch(); return 0; }
+
+queue<char> to_revpol(const string& in)
+{
+	stack<char> stk;
+	queue<char> gen;
+	for (auto i : in)
+	{
+		if (i == '.')
+			gen.push(i);
+		else if ((i >= '0' && i <= '9') ||
+			(i >= 'a' && i <= 'z') ||
+			(i >= 'A' && i <= 'Z'))
+		{
+			if (gen.empty() != true &&
+				(gen.back() >= '0' && gen.back() <= '9') &&
+				!(i >= '0' && i <= '9'))
+			{
+				stk.push('*'); gen.push(';');
+			}
+			gen.push(i);
+		}
+		else if
+			(i == '+' || i == '-' ||
+				i == '*' || i == '/')
+		{
+			if (stk.empty() != true && (stk.top() == '*' || stk.top() == '/'))
+			{
+				if (i == '*' || i == '/')
+				{
+					gen.push(stk.top());
+					stk.top() = i;
+				}
+				else if (i == '+' || i == '-')
+				{
+					while (stk.empty() != true && stk.top() != '(')
+					{
+						gen.push(stk.top());
+						stk.pop();
+					}
+					stk.push(i);
+				}
+			}
+			else
+			{
+				if (gen.empty() != true && (
+					(gen.back() >= '0' && gen.back() <= '9') ||
+					(gen.back() >= 'a' && gen.back() <= 'z') ||
+					(gen.back() >= 'A' && gen.back() <= 'Z')))
+					gen.push(';');
+				else if (((stk.empty() != true && stk.top() == '(') || gen.empty() == true) && i == '-')
+					gen.push('_');
+				if (gen.back() != '_')
+					stk.push(i);
+			}
+		}
+		else if (i == '(')
+		{
+			if (gen.empty() != true && (
+				(gen.back() >= '0' && gen.back() <= '9') ||
+				(gen.back() >= 'a' && gen.back() <= 'z') ||
+				(gen.back() >= 'A' && gen.back() <= 'Z')))
+			{
+				gen.push(';');
+				stk.push('*');
+			}
+			stk.push(i);
+		}
+		else if (i == ')')
+		{
+			while (stk.empty() != true && stk.top() != '(')
+			{
+				gen.push(stk.top());
+				stk.pop();
+			}
+			if (stk.empty() != true)
+				stk.pop();
+			else
+				throw "worng input, Missing (";
+		}
+	}
+
+	while (stk.empty() != true)
+	{
+		gen.push(stk.top());
+		stk.pop();
+	}
+	return gen;
+}
+
+double calc(queue<char> in)
+{
+	stack<double> res;
+	bool minus = false, op = false;
+	size_t fl = 0;
+	double temp;
+
+	res.push(0.0);
+	while (in.empty() != true)
+	{
+		if (in.front() == '_')
+		{
+			minus = true;
+			in.pop();
+		}
+		else if (in.front() >= '0' && in.front() <= '9')
+		{
+			if (op == true) { res.push(0.0); op = false; }
+			while (in.empty() != true && in.front() >= '0' && in.front() <= '9')
+			{
+				if ((in.front() == '0' && res.top() == 0.0) == false)
+				{
+					if (fl == 0)
+					{
+						res.top() = res.top() * 10 + in.front() - 48;
+					}
+					else
+					{
+						temp = in.front() - 48;
+						for (size_t i = 0; i < fl; i++)
+							temp /= 10;
+						res.top() += temp;
+						fl++;
+					}
+				}
+				in.pop();
+				if (in.empty() != true && in.front() == '.')
+				{
+					fl = 1;
+					in.pop();
+				}
+			}
+			fl = 0;
+		}
+		else if ((in.front() >= 'a' && in.front() <= 'z') || (in.front() >= 'A' && in.front() <= 'Z'))
+		{
+			cout << "Please enter the variable \'";
+			do
+			{
+				cout << in.front();
+				in.pop();
+			} while (in.empty() != true && (in.front() >= 'a' && in.front() <= 'z') || (in.front() >= 'A' && in.front() <= 'Z'));
+			cout << "\': ";
+			if (op == true) { res.push(0.0); op = false; }
+			cin >> res.top();
+		}
+		else if (in.front() == ';' ||
+			in.front() == '+' || in.front() == '-' ||
+			in.front() == '*' || in.front() == '/')
+		{
+			if (minus == true)
+			{
+				minus = false;
+				res.top() = -res.top();
+			}
+			if (in.front() == ';')
+			{
+				res.push(0.0);
+				in.pop();
+			}
+			else if (res.size() > 1)
+			{
+				temp = res.top();
+				res.pop();
+				switch (in.front())
+				{
+				case '+':
+					res.top() += temp;
+					break;
+				case '-':
+					res.top() -= temp;
+					break;
+				case '*':
+					res.top() *= temp;
+					break;
+				case '/':
+					res.top() /= temp;
+					break;
+				}
+				op = true;
+				in.pop();
+			}
+		}
+	}
+	if (res.size() > 1)
+	{
+		cout << "something went wrong!";
+		while (res.empty() != true)
+		{
+			cout << res.top() << ' ';
+			res.pop();
+		}
+		return 0.0;
+	}
+	return res.top();
+}
+template <class T>
+ostream& operator<<(ostream& out, queue<T> cs)
+{
+	while (cs.empty() != true)
+	{
+		out << cs.front();
+		cs.pop();
+	}
+	return out;
+}
+
+int main()
+{
+	string in;
+	queue<char> gen;
+	cout << "Enter an expression:\n";
+	getline(cin, in);
+
+	gen = to_revpol(in);
+	cout << "Result of the function to_revpol: " << gen << endl;
+	cout << "Final result: " << calc(gen);
+	return end();
+}
---
+++ lab7/lab7/lab7.cpp
@@ -0,0 +1,204 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+const int n = 5;
+
+void FindD(float A[n][n], float S[n][n], float D[n][n], int i)
+{
+	float c = 0, b = 0;
+	int l;
+	for (l = 0; l <= i - 1; l++) c = c + S[l][i] * S[l][i] * D[l][l];
+	b = A[i][i] - c;
+	if (b == 0) D[i][i] = 0; else  if (b < 0) D[i][i] = -1; else D[i][i] = 1;
+}
+
+void FindSij(float A[n][n], float S[n][n], float D[n][n], int i, int j)
+{
+	float c = 0;
+	int l;
+	for (l = 0; l <= i - 1; l++) c = c + S[l][i] * D[l][l] * S[l][j];
+	if ((S[i][i] == 0) || (D[i][i] == 0)) S[i][j] = 0; else S[i][j] = (A[i][j] - c) / S[i][i] * D[i][i];
+}
+
+void FindSii(float A[n][n], float S[n][n], float D[n][n], int i)
+{
+	float c = 0;
+	int l;
+	for (l = 0; l <= i - 1; l++) c = c + S[l][i] * S[l][i] * D[l][l];
+	S[i][i] = sqrt(fabs(A[i][i] - c));
+}
+
+void Findy(float A[n][n], float St[n][n], float b[n], float y[n], int i)
+{
+	float c = 0;
+	int k;
+	for (k = 0; k <= i - 1; k++)  c = c + St[i][k] * y[k];
+	y[i] = (b[i] - c) / St[i][i];
+}
+
+void Findx(float A[n][n], float S[n][n], float D[n][n], float y[n], float x[n], int i)
+{
+	float c = 0;
+	int k;
+	for (k = i + 1; k < n; k++) c = c + D[i][i] * S[i][k] * x[k];
+	x[i] = (y[i] - c) / (D[i][i] * S[i][i]);
+}
+
+void FindDet(float S[n][n], float D[n][n], float& DetA)
+{
+	float c = 1, o = 1;
+	int k;
+	for (k = 0; k < n; k++) c = c * S[k][k];
+	for (k = 0; k < n; k++) o = o * D[k][k];
+	DetA = o * (c * c);
+}
+
+void Umn(float A[n][n], float x[n], float b[n], float r[n])
+{
+	float c = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++) c = c + (A[i][j] * x[j]);
+		r[i] = c - b[i];
+		c = 0;
+	}
+}
+
+void Umnm(float A[n][n], float S[n][n], float M[n][n])
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			M[i][j] = 0;
+			for (int k = 0; k < n; k++) M[i][j] = M[i][j] + (A[i][k] * S[k][j]);
+		}
+	}
+}
+
+int main()
+{
+	const int n = 5;
+	float A[n][n];
+	float S[n][n], St[n][n], D[n][n];
+	float b[n];
+	int i, j;
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+		{
+			S[i][j] = 0;
+			St[i][j] = 0;
+			D[i][j] = 0;
+		}
+	cout << "Vvedite matricu A " << n << "x" << n << ": " << endl;
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++) cin >> A[i][j];
+	cout << "Vvedite vector B razmera " << n << ": " << endl;
+	for (i = 0; i < n; i++) cin >> b[i];
+	cout << "A = " << endl;
+	cout << setprecision(6) << fixed;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++) cout << A[i][j] << " ";
+		cout << endl;
+	}
+	cout << "b = " << endl;
+	for (i = 0; i < n; i++) cout << b[j] << " ";
+	cout << endl;
+	bool B = true;
+	for (i = 0; i < n; i++)
+	{
+		for (j = i; j < n; j++)
+			if (i == j)
+			{
+				FindSii(A, S, D, i);
+				FindD(A, S, D, i);
+				if ((S[i][i] == 0) || (D[i][i] == 0)) B = false;
+			}
+			else FindSij(A, S, D, i, j);
+	}
+	if (B == false)
+	{
+		cout << "Nedopustimaya matrica!DetA = 0" << endl; return 0;
+	}
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++) St[i][j] = S[j][i];
+	cout << "S = " << endl;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++) cout << S[i][j] << " ";
+		cout << endl;
+	}
+	cout << "D = " << endl;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++) cout << D[i][j] << " ";
+		cout << endl;
+	}
+	cout << "St = " << endl;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++) cout << St[i][j] << " ";
+		cout << endl;
+	}
+
+	float y[n], x[n];
+	for (i = 0; i < n; i++) Findy(A, St, b, y, i);
+	for (i = n - 1; i >= 0; i--) Findx(A, S, D, y, x, i);
+	cout << "y = " << endl;
+	for (i = 0; i < n; i++) cout << y[i] << " ";
+	cout << endl;
+	cout << "x = " << endl;
+	for (i = 0; i < n; i++) cout << x[i] << " ";
+	cout << endl;
+	float DetA;
+	FindDet(S, D, DetA);
+	cout << "Det A = " << DetA << endl;
+	float H[n][n];
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++) H[i][j] = 0;
+	int m;
+	float z[n], h[n], e[n];
+	for (m = 0; m < n; m++)
+	{
+		for (i = 0; i < n; i++) e[i] = 0;
+		e[m] = 1;
+		for (i = 0; i < n; i++) Findy(A, St, e, z, i);
+		for (i = n - 1; i >= 0; i--) Findx(A, S, D, z, h, i);
+		for (i = 0; i < n; i++) H[i][m] = h[i];
+	}
+	cout << "H = " << endl;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++) cout << H[i][j] << " ";
+		cout << endl;
+	}
+	cout << "NEVYAZKI: " << endl;
+	float r[n];
+	Umn(A, x, b, r);
+	cout << "Ax - b = " << endl;
+	for (i = 0; i < n; i++) cout << r[i] << " ";
+	cout << endl;
+	float M[n][n], F[n][n];
+	Umnm(D, S, M);
+	Umnm(St, M, F);
+	cout << "St * D * S = " << endl;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++) cout << F[i][j] << " ";
+		cout << endl;
+	}
+	float N1[n][n];
+	Umnm(A, H, N1);
+	cout << "A * H = " << endl;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++) cout << N1[i][j] << " ";
+		cout << endl;
+	}
+	return 0;
+}
